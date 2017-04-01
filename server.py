a67f2039377b8c21e465b89916cf6e9fa6a64e53
MishaLarionov <MishaLarionov@users.noreply.github.com>
2017-04-01 17:25:52 -0400

    p o s t / g e t

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, url_for
 app = Flask(__name__)
 
-@app.route('/<name>')
-def hello(name=None):
-    return render_template('hello.html', name=name)
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return render_template('hello.html', name=request.args.get("name"))
+    else:
+        return render_template('hello.html', name=request.args.get("name"))
