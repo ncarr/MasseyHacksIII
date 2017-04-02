d4c48e9456c45e03faaa509f855d20e3f037137e
Nicholas Carr <nicholas.carr@rogers.com>
2017-04-02 01:18:53 -0400

    New API

--- server.py
+++ server.py
@@ -1,31 +1,33 @@
 from flask import Flask, render_template, request, url_for
 from flask_mongoengine import MongoEngine
-import datetime
+import datetime, requests
 app = Flask(__name__)
 app.config.from_pyfile('cfg.py')
 db = MongoEngine(app)
 
-@app.route('/addProduct')
-def addProduct():
-    return render_template("form.html")
-
-@app.route('/api/uploadfood', methods=['POST'])
-def uploadfood():
+@app.route('/', methods=['GET', 'POST'])
+def login():
     if request.method == 'POST':
-        Food(name=request.form['name'], upc=request.form['upc'], expiry=datetime.datetime.strptime(request.form['expiry'], '%Y-%m-%d')).save()
+        return render_template('hello.html', name=request.args.get("name"))
+    else:
+        return render_template('hello.html', name=request.args.get("name"))
 
-class Food(db.Document):
+@app.route('/fruitinfo')
+def fruitInfo():
+    appleInfo = Nutrition.getFruit('1 large apple')
+    return render_template('fruitInfo.html', name=appleInfo['ingredients'][0]['text'], calories=appleInfo['calories'], imageUrl='https://staticdelivery.nexusmods.com/mods/110/images/74627-0-1459502036.jpg')
+
+@app.route('/api/fruits', methods=['POST'])
+def uploadFruit():
+    if request.method == 'POST':
+        Fruit(name=request.form['name'], upc=request.form['upc'], expiry=datetime.datetime.strptime(request.form['expiry'], '%Y-%m-%d')).save()
+
+class Fruit(db.Document):
     name = db.StringField(required=True)
     upc = db.StringField()
     expiry = db.DateTimeField()
-    #fridge = db.ReferenceField(Fridge, reverse_delete_rule=CASCADE)
 
-"""
-class Fridge(db.Document):
-    name = db.StringField(required=True)
-    owner = db.ReferenceField(User, reverse_delete_rule=CASCADE)
-
-class User(db.Document):
-    name = db.StringField(required=True)
-    fridges = db.List
-"""
+class Nutrition(object):
+    def getFruit(ingredient):
+        r = requests.get('https://api.edamam.com/api/nutrition-data?app_id=' + app.config.get('EDAMAM_APP_ID') + '&app_key=' + app.config.get('EDAMAM_APP_KEY') + '&ingr=' + ingredient)
+        return r.json()
