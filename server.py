7929d047e88305a03385abeb376c109e00203062
Nicholas Carr <nicholas.carr@rogers.com>
2017-04-02 09:06:04 -0400

    Added separate fruit list page

--- server.py
+++ server.py
@@ -1,22 +1,19 @@
 from flask import Flask, render_template, request, url_for
 from flask_mongoengine import MongoEngine
 import datetime, requests, csv
-app = Flask(__name__)
+app = Flask(__name__, static_folder='public', static_url_path='')
 app.config.from_pyfile('cfg.py')
 db = MongoEngine(app)
 
-fruitlist = {'name':[], 'url':[]}
+fruitlist = {}
 with open('fruits.csv', newline='') as csvfile:
     fruitreader = csv.reader(csvfile)
     for row in fruitreader:
         fruitlist[row[0]] = row[1]
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        return render_template('index.html', name=request.args.get("name"))
-    else:
-        return render_template('index.html', name=request.args.get("name"))
+@app.route('/fruits')
+def addFruit():
+    return render_template('fruits.html', fruits=fruitlist)
 
 @app.route('/fruitinfo/<name>')
 def fruitInfo(name):
@@ -27,9 +24,15 @@ def fruitInfo(name):
         imageUrl = 'https://staticdelivery.nexusmods.com/mods/110/images/74627-0-1459502036.jpg'
     return render_template('fruitInfo.html', name=name, calories=appleInfo['calories'], imageUrl=imageUrl)
 
+@app.route('/api/fruits', methods=['POST'])
+def uploadFruit():
+    if request.method == 'POST':
+        Fruit(name=request.form['name'], upc=request.form['upc'], expiry=datetime.datetime.strptime(request.form['expiry'], '%Y-%m-%d')).save()
+
 class Fruit(db.Document):
     name = db.StringField(required=True)
-    qty = db.IntField(required=True)
+    upc = db.StringField()
+    expiry = db.DateTimeField()
 
 class Nutrition(object):
     def getFruit(ingredient):
